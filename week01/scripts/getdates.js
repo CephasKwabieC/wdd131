ae4f982092f3495e9afb68ca606c01961dbaac77
CephasKwabieC <cephaskwabiec@gmail.com>
2025-07-05 15:00:57 +0000

    aboutMe page

---
+++ week01/scripts/getdates.js
@@ -0,0 +1,5 @@
+// set current year
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+// set last modified date
+document.getElementById("lastModified").textContent = "Last Modified: " + document.lastModified;
